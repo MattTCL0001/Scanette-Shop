e1d800a81d653edb9fe781db447cd5f3554bf227
Matt TCL <croitoru.matthew@gmail.com>
2026-06-07 13:38:00 +0200

    Update Liste.js

--- Liste.js
+++ Liste.js
@@ -9,6 +9,6 @@ const baseProduits = {
     "87345087": { nom: "Mentos goût Cola - 37.5g", prix: 2.00 },
     "7622202300974 ": { nom: "Chocolat blanc Toblerone - 340g", prix: 2.00 },
     "5449000308016": { nom: "Coca Cola sans sucre - verre 750mL", prix: 2.00 },
-    "5449000233295": { nom: "Coca Cola sans sucre sans caféine - canette 330mL", prix: 2.00 }
+    "5449000233295": { nom: "Coca Cola sans sucre sans caféine - canette 330mL", prix: 2.00 },
     "4000512008903 ": { nom: "Trolli Big Burger", prix: 1.00 },
 };
