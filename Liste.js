293f1d5fa79edb77f6370c8462b97463140bf0ae
Matt TCL <croitoru.matthew@gmail.com>
2026-06-07 13:35:51 +0200

    Update Liste.js

--- Liste.js
+++ Liste.js
@@ -6,8 +6,9 @@ const baseProduits = {
     "8886467125614": { nom: "Pringles Tangy Tomato Twist - 102g", prix: 2.00 },
     "3086123700970": { nom: "Coffret 9 produits BIC x Super Mario", prix: 2.00 },
     "3780402606990": { nom: "Magazine Lego Minecraft", prix: 6.99 },
-"87345087": { nom: "Mentos goût Cola - 37.5g", prix: 2.00 },
-"7622202300974 ": { nom: "Chocolat blanc Toblerone - 340g", prix: 2.00 },
+    "87345087": { nom: "Mentos goût Cola - 37.5g", prix: 2.00 },
+    "7622202300974 ": { nom: "Chocolat blanc Toblerone - 340g", prix: 2.00 },
     "5449000308016": { nom: "Coca Cola sans sucre - verre 750mL", prix: 2.00 },
     "5449000233295": { nom: "Coca Cola sans sucre sans caféine - canette 330mL", prix: 2.00 }
+    "4000512008903 ": { nom: "Trolli Big Burger", prix: 1.00 },
 };
